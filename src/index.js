974b7c831ff6343ad9e986e0dc868e3cb5b0f8d9
abdulhadishueb <abdulhadishueb@gmail.com>
2025-12-18 10:02:34 +0300

    Pass tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    
+    const task = document.querySelector(
+      'input[name="new-task-description"]'
+    ).value;
+
+    buildToDo(task);
+    form.reset();
+  });
+
+  function buildToDo(task) {
+    const taskList = document.getElementById("tasks");
+    const li = document.createElement("li");
+    li.textContent = task;
+    taskList.appendChild(li);
+  }
 });
